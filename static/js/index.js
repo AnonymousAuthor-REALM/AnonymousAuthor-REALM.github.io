9d45f92bd48acfe28cc3a736cf7aa06cb1217158
AnonymousAuthor-REALM <ano_realm@163.com>
2025-06-29 10:37:38 +0800

    change template to GAP3DS

--- static/js/index.js
+++ static/js/index.js
@@ -1,78 +1,68 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
-
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
-
 
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-    });
 
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 3,
+			slidesToShow: 1,
 			loop: true,
 			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
+			autoplay: true,
+			autoplaySpeed: 5000,
     }
 
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
+	
     bulmaSlider.attach();
 
 })
+document.addEventListener('DOMContentLoaded', function () {
+	const carousel = bulmaCarousel.attach('#results-carousel', {
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  loop: false,
+	  autoplay: false,
+	});
+  
+	const thumbnailContainer = document.querySelector('.thumbnail-container');
+  
+	if (carousel && thumbnailContainer) {
+	  const items = carousel.items;
+  
+	  // 动态创建缩略图
+	  items.forEach((item, index) => {
+		const img = item.element.querySelector('img');
+		if (!img) return;
+  
+		const thumb = document.createElement('img');
+		thumb.src = img.src;
+		thumb.alt = img.alt;
+		thumb.classList.add('thumbnail');
+		if (index === 0) thumb.classList.add('is-active');
+  
+		thumb.addEventListener('click', () => {
+		  carousel.goTo(index);
+		  updateThumbnails(index);
+		});
+  
+		thumbnailContainer.appendChild(thumb);
+	  });
+  
+	  // 更新当前缩略图高亮状态
+	  function updateThumbnails(currentIndex) {
+		const thumbs = thumbnailContainer.querySelectorAll('.thumbnail');
+		thumbs.forEach((thumb, idx) => {
+		  thumb.classList.remove('is-active');
+		  if (idx === currentIndex) thumb.classList.add('is-active');
+		});
+	  }
+  
+	  // 监听轮播切换事件更新缩略图
+	  carousel.on('before:change', (event) => {
+		updateThumbnails(event.index);
+	  });
+	}
+  });
